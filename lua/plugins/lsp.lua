8bc02e3768575e4f43fd54340c80cfe9ce2efd04
totoro1302 <garichard85@gmail.com>
2026-06-05 12:41:05 +0200

    enable rust analyzer, move vim config into a separate file

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -23,11 +23,13 @@ return {
         config = function()
             vim.lsp.enable("gopls")
             vim.lsp.enable("lua_ls")
-
+            vim.lsp.enable("rust_analyzer")
             vim.api.nvim_create_autocmd("LspAttach", {
                 callback = function(event)
                     local opts = { buffer = event.buf, silent = true }
                     local builtin = require("telescope.builtin")
+                    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                    vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
                     vim.keymap.set("n", "gd", builtin.lsp_definitions, opts)
                     vim.keymap.set("n", "gr", builtin.lsp_references, opts)
                     vim.keymap.set("n", "gi", builtin.lsp_implementations, opts)
