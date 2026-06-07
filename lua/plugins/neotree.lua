df6467a919b4f52d129ada187926a049418dd6e5
Totoro1302 <garichard85@gmail.com>
2026-06-07 15:13:26 +0200

    Add linter & formatter (null-ls) and lualine (#4)

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,42 +1,42 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "MunifTanjim/nui.nvim",
-        "nvim-tree/nvim-web-devicons",
-    },
-    lazy = false,
-    config = function()
-        require("neo-tree").setup({
-            close_if_last_window = true,
-            filesystem = {
-                use_libuv_file_watcher = true,
-                filtered_items = {
-                    visible = true,
-                    never_show = {
-                        ".DS_Store",
-                        "thumbs.db",
-                        ".git",
-                    },
-                },
-                follow_current_file = {
-                    enabled = true,
-                },
-            },
-            window = {
-                position = "left",
-                width = 40,
-            },
-        })
-        vim.api.nvim_create_autocmd("VimEnter", {
-            callback = function()
-                require("neo-tree.command").execute({
-                    toggle = false,
-                    dir = vim.loop.cwd(),
-                })
-                vim.keymap.set('n', '<leader>n', ":Neotree toggle<CR>", { silent = true })
-            end,
-        })
-    end
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		"nvim-tree/nvim-web-devicons",
+	},
+	lazy = false,
+	config = function()
+		require("neo-tree").setup({
+			close_if_last_window = true,
+			filesystem = {
+				use_libuv_file_watcher = true,
+				filtered_items = {
+					visible = true,
+					never_show = {
+						".DS_Store",
+						"thumbs.db",
+						".git",
+					},
+				},
+				follow_current_file = {
+					enabled = true,
+				},
+			},
+			window = {
+				position = "left",
+				width = 40,
+			},
+		})
+		vim.api.nvim_create_autocmd("VimEnter", {
+			callback = function()
+				require("neo-tree.command").execute({
+					toggle = false,
+					dir = vim.loop.cwd(),
+				})
+				vim.keymap.set("n", "<leader>n", ":Neotree toggle<CR>", { silent = true })
+			end,
+		})
+	end,
 }
